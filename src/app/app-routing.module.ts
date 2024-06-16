af920d4e9fa42591bb6066b5c242fabfa31ce253
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2024-06-16 21:55:38 +0300

    feat: add initial components and set up basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KnowledgeComponent } from './features/knowledge/knowledge.component';
+import { CalculatorComponent } from './features/calculator/calculator.component';
+import { CollectionComponent } from './features/collection/collection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/knowledge', pathMatch: 'full' },
+  { path: 'knowledge', component: KnowledgeComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'collection', component: CollectionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { CalculatorComponent } from './features/calculator/calculator.component';
+import { CollectionComponent } from './features/collection/collection.component';
+import { KnowledgeComponent } from './features/knowledge/knowledge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CalculatorComponent,
+    CollectionComponent,
+    KnowledgeComponent
   ],
   imports: [
     BrowserModule,
